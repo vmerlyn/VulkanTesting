a4a737d11ad4c3bc386396c848ba89dcafd045ed
Vibeesh Bose <vibeesh.bose@merlynsoftware.com>
2024-02-18 19:23:41 -0600

    Initial XCode Project

---
+++ VulkanTesting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VulkanTesting
+//
+//  Created by Vibeesh Bose on 2/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
